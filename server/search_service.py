c20b2296fa42a99f404bd548cecef51cdeac96b8
zoo <anfdnfl@khu.ac.kr>
2024-12-08 13:17:45 +0900

    주석 수정

--- server/search_service.py
+++ server/search_service.py
@@ -4,28 +4,28 @@ from func_collections import getDatabyKeyword, getSimilar, getKeyword, getVideo
 app = Flask(__name__)
 
 
-# 검색 키워드를 통해 영화, TV 프로그램 데이터 불러오기
+# 검색 키워드를 통해 영화, TV 프로그램 데이터 찾기
 @app.route("/search/<keyword>/<int:page>", methods=["GET"])
 def search(keyword, page):
     datas = getDatabyKeyword(keyword, page)
     return jsonify(datas)
 
 
-# 비슷한 영화, TV 프로그램 데이터 불러오기
+# 비슷한 영화, TV 프로그램 데이터 찾기
 @app.route("/similar/<media>/<id>/<int:page>", methods=["GET"])
 def similar(media, id, page):
     datas = getSimilar(media, id, page)
     return jsonify(datas)
 
 
-# 키워드 검색
+# 키워드 찾기
 @app.route("/keyword/<media>/<id>", methods=["GET"])
 def keyword(media, id):
     datas = getKeyword(media, id)
     return jsonify(datas)
 
 
-# 티저 영상 검색
+# 티저 영상 찾기
 @app.route("/video/<media>/<id>", methods=["GET"])
 def video(media, id):
     datas = getVideo(media, id)
